98231fd8b0d97b040d660bafe518bd7c8002006b
oluwatobi2001 <tobilyn77@gmail.com>
2025-02-04 21:30:08 +0100

    project

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project Description
+
+This is simple REST API project for the stage 0 of the HNG program. It  generates the basic user details when fetched. 
+
+## How to run the project locally
+to run the project locally, 
+* CLone the prohect github repo or download it to your local machine
+* Run `npm install` or `npm i`  to download all the necessary dependencies needed to power the backend server.
+* Finally, run the command `node index` to kickstart the server. 
+* You can access the server via a web browser or use Postman to see the expected response
+
+
+
+## API Documentation
+
+Here is the link to the endpoint URL 
+<https://hng-stage0-9oat.onrender.com>
+<>
+Here is the expected response 
+
+```
+{
+    "email":"tobilyn77@gmail.com","current_datetime":"2025-01-31T06:52:13.634Z",
+"github_url":"<https://github.com/oluwatobi12001/hng_stage0>"
+}
+```
+## Further Info 
+Check out this link to learn more about Node JS
+
+[HNG Node JS](https://hng.tech/hire/nodejs-developers)
---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const axios = require("axios");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+const checkPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
+
+const checkPerfect = (num) => {
+    if (num <= 1) return false;
+    let sumOfDivisor = 1;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            sumOfDivisor += i;
+            if (i !== num / i) sumOfDivisor += num / i; // Add pair divisor
+        }
+    }
+    return sumOfDivisor === num;
+};
+
+const sumDigit = (num) => {
+    return num.toString().split("").reduce((acc, digit) => acc + parseInt(digit), 0);
+};
+
+const checkArmstrong = (num) => {
+    const digits = num.toString().split("");
+    const power = digits.length;
+    const sum = digits.reduce((acc, digit) => acc + Math.pow(parseInt(digit), power), 0);
+    if (sum === num) {
+        return "armstrong"
+    } 
+    return ;
+};
+
+const checkOdd = (num) => {
+    return num % 2 === 0 ? "even" : "odd";
+};
+
+const funFact = async (num) => {
+    try {
+        const api_url = `http://numbersapi.com/${num}/trivia`;
+        const response = await axios.get(api_url);
+        return response.data;
+    } catch (error) {
+        return "No fun fact available.";
+    }
+};
+
+// ✅ FIXED ROUTE
+app.get("/hello", async (req, res) => {
+    try {
+        const { number } = req.query;
+        const numberFormatted = parseInt(number, 10);
+
+        if (isNaN(numberFormatted)) {
+            return res.status(400).json({
+                "number": "alphabet",
+                "error": true
+            });
+        }
+
+        const fact = await funFact(numberFormatted); 
+     
+        const propertiesSection =(numberFormatted)=>{
+            checkOdd(numberFormatted)
+            if(checkArmstrong(numberFormatted)) {
+                return ["armstrong", checkOdd(numberFormatted)]
+            }
+            else {
+                return[checkOdd(numberFormatted)]
+            }
+
+        }
+
+        res.status(200).json({
+            "number": numberFormatted,
+            "is_prime": checkPrime(numberFormatted),
+            "is_perfect": checkPerfect(numberFormatted),
+            "properties": propertiesSection(numberFormatted),
+            "digit_sum": sumDigit(numberFormatted),
+            "fun_fact": fact
+        });
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            "error": "Internal Server Error"
+        });
+    }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running...");
+});
